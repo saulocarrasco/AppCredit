98cad63e1478e63f22a696dc5385e74b64331baf
unknown <saulocperez@gmail.com>
2019-08-24 16:04:26 -0400

    Adding testing project

---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class AddressServiceTest
+    {
+        [TestMethod]
+        public void AddAddressTest()
+        {
+        }
+    }
+}
