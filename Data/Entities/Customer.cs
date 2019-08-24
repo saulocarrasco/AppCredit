2ee142a5d9e2cc086df281a4a85fc69b338d5a83
unknown <saulocperez@gmail.com>
2019-08-24 17:17:07 -0400

    Refactoring generic repository

--- Data/Entities/Customer.cs
+++ Data/Entities/Customer.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Data.Entities
 {
-    public class Customer : Person, ITransactEntity
+    public class Customer : Person, ITransactionEntity
     {
         public int Id { get; set; }
         public bool IsDeleted { get; set; }
--- Domain/Services/GenericService.cs
+++ Domain/Services/GenericService.cs
@@ -8,39 +8,8 @@ using System.Threading.Tasks;
 
 namespace Domain.Services
 {
-    public class GenericService<TModel> : ITransactionService<TModel> where TModel : class, ITransactionEntity
+    public class GenericService : TransactionService
     {
-        private readonly DbContext _dbContext;
-        public GenericService(DbContext dbContext) => _dbContext = dbContext;
-
-        public void Delete(TModel model)
-        {
-            model.IsDeleted = true;
-            _dbContext.Entry(model).State = EntityState.Modified;
-        }
-
-        public async Task<IEnumerable<TModel>> GetAll(string includes)
-        {
-            var result = await _dbContext.Set<TModel>().Include(includes).ToArrayAsync();
-
-            return result;
-        }
-
-        public async Task<TModel> Insert(TModel model)
-        {
-            await _dbContext.Set<TModel>().AddAsync(model);
-
-            return model;
-        }
-
-        public async Task<int> SavesChanges()
-        {
-            return await _dbContext.SaveChangesAsync();
-        }
-
-        public void Update(TModel model)
-        {
-            _dbContext.Entry(model).State = EntityState.Modified;
-        }
+        
     }
 }
