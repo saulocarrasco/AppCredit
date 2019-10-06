3efd4e02204defd44af7525991ead13f104165d5
unknown <saulocperez@gmail.com>
2019-10-06 14:36:15 -0400

    Add vue component to show customers

---
+++ FrontPage/wwwroot/js/customer/index.js
@@ -0,0 +1,59 @@
+﻿var customerVue = new Vue({
+    el: "#rootElement",
+    components: {
+
+    },
+    data: function () {
+        return {
+           
+            customers: [],
+            sectionText: "Registro de Fianzas",
+            dateHelper: Object,
+            addressHelper: Object
+        };
+    },
+    methods: {
+       
+        loadCustomers: function () {
+
+            var self = this;
+
+            const config = { headers: { 'Content-Type': 'application/json' } };
+
+            var endPoint = '/customer/';
+
+            instance.get(
+                endPoint,
+                config
+            ).then(function (response) {
+
+                self.customers = response.data;
+
+            }).catch(function (errors) {
+
+                console.log(errors);
+
+            });
+
+        }
+
+    },
+    mounted: function () {
+        var self = this;
+        self.dateHelper = new DateHelper();
+        self.addressHelper = new AddressHelper();
+        self.loadCustomers();
+    },
+    watch: {
+        //"dataForm.appSelected": function (value) {
+        //    this.onSelectedAppHandler();
+        //},
+    }
+    //dataForm: {
+    //    handler: function () {
+
+    //    },
+    //    deep: true
+    //}
+    //}
+});
