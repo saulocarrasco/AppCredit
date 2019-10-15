16dfc5c75a33aaf409b7b766e67cb0ed7f6d0b78
SAULO CARRASCO <saulo_carrasco@bhdleon.com.do>
2019-10-15 17:16:15 -0400

    add endpoint to register user

---
+++ AppCredit/Migrations/20191015183333_AddAuthEntities.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppCredit.Api.Migrations
+{
+    public partial class AddAuthEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedDate = table.Column<DateTimeOffset>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedDate = table.Column<DateTimeOffset>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    CreationDate = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedDate = table.Column<DateTimeOffset>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    RoleId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Permissions_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_RoleId",
+                table: "Permissions",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_UserId",
+                table: "Roles",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ FrontPage/wwwroot/js/user/login.js
@@ -0,0 +1,55 @@
+﻿var userVue = new Vue({
+    el: "#rootElement",
+    components: {
+
+    },
+    data: function () {
+        return {
+            dataModel: {
+                userNameOrEmail: "",
+                passWord: ""
+            }
+        };
+    },
+    methods: {
+        saveUser: function (e) {
+
+            e.preventDefault();
+
+            var self = this;
+
+            const config = { headers: { 'Content-Type': 'application/json' } };
+
+            var endPoint = 'authentication/login/';
+
+            instance.post(
+                endPoint,
+                self.dataModel,
+                config
+            ).then(function (response) {
+
+                window.location = "/Account/";
+
+            }).catch(function (errors) {
+
+                console.log(errors);
+
+            });
+
+        }
+    },
+    mounted: function () {
+    },
+    watch: {
+        //"dataForm.appSelected": function (value) {
+        //    this.onSelectedAppHandler();
+        //},
+    }
+    //dataForm: {
+    //    handler: function () {
+
+    //    },
+    //    deep: true
+    //}
+    //}
+});
